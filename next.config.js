0a983af5cc3f5cd414400b675043dd1954a496e9
caamillo <diaskygotah@gmail.com>
2025-07-11 20:18:06 +0200

    gh actions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true, // required for static export if you use next/image
+  },
+  basePath: '/tua', // only needed if deploying to a subfolder
+}
 
 module.exports = nextConfig
